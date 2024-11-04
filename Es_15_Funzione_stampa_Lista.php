26925db0a4ee40f97dde01071f35caccbf42ff8c
Matteo <matteo.ciardi26@gmail.com>
2024-11-04 18:01:35 +0100

    es finito

--- Es_15_Funzione_stampa_Lista.php
+++ Es_15_Funzione_stampa_Lista.php
@@ -9,21 +9,26 @@
     
     <?php 
         
-        function stampaLista($num, $tipo){
+        function stampaLista($num, $tipolista){
+            if ($tipolista == "o") {
+                $tipo = "ol";
+            } elseif ($tipolista == "u") {
+                $tipo = "ul";
+            } else {
+                echo "errore";
+            }
             
-        $str="<".$tipo.">";
+            $str="<".$tipo.">";
             for ($i=1; $i <= $num; $i++) { 
                 if($i%5==0){
                     $str=$str."<li>".$i."</li>";
-                }
-                
+                }      
             }
             $str=$str . "</".$tipo.">";
             return $str;
         }
-        $lista = stampaLista(40,"ul");
+        $lista = stampaLista(40,"u");
         echo "$lista";
-
     
     ?>
 </body>
